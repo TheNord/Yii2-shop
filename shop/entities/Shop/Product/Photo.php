31338b7535225d64371e896579c3bc732f58d298
thenord <dmr11@bk.ru>
2018-12-23 22:33:12 +0300

    Installed image upload behavior

--- shop/entities/Shop/Product/Photo.php
+++ shop/entities/Shop/Product/Photo.php
@@ -4,11 +4,14 @@ namespace shop\entities\Shop\Product;
 
 use yii\db\ActiveRecord;
 use yii\web\UploadedFile;
+use yiidreamteam\upload\ImageUploadBehavior;
 
 /**
  * @property integer $id
  * @property string $file
  * @property integer $sort
+ *
+ * @mixin ImageUploadBehavior
  */
 class Photo extends ActiveRecord
 {
@@ -32,5 +35,29 @@ class Photo extends ActiveRecord
     public static function tableName(): string
     {
         return '{{%shop_photos}}';
+    }
+
+    public function behaviors(): array
+    {
+        return [
+            [
+                // подключаем ImageUploadBehavior
+                'class' => ImageUploadBehavior::className(),
+                // отслеживаем поле file
+                'attribute' => 'file',
+                // генерируем превьюшки по запросу, генерируем только при отсутствии
+                'createThumbsOnRequest' => true,
+                // указываем пути к статике
+                'filePath' => '@staticRoot/origin/products/[[attribute_product_id]]/[[id]].[[extension]]',
+                'fileUrl' => '@static/origin/products/[[attribute_product_id]]/[[id]].[[extension]]',
+                'thumbPath' => '@staticRoot/cache/products/[[attribute_product_id]]/[[profile]]_[[id]].[[extension]]',
+                'thumbUrl' => '@static/cache/products/[[attribute_product_id]]/[[profile]]_[[id]].[[extension]]',
+                // размеры превьюшек, для админ панели и основные
+                'thumbs' => [
+                    'admin' => ['width' => 100, 'height' => 70],
+                    'thumb' => ['width' => 640, 'height' => 480],
+                ],
+            ],
+        ];
     }
 }
