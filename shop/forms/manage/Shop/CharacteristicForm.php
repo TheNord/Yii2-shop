7f0650df789505c0b7aa35fddf8ee582bdb3ee79
thenord <dmr11@bk.ru>
2018-12-23 13:28:46 +0300

    Added characteristic

---
+++ shop/forms/manage/Shop/CharacteristicForm.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace shop\forms\manage\Shop;
+
+use shop\entities\Shop\Characteristic;
+use yii\base\Model;
+
+/**
+ * @property array $variants
+ */
+class CharacteristicForm extends Model
+{
+    public $name;
+    public $type;
+    public $required;
+    public $default;
+    public $textVariants;
+    public $sort;
+    private $_characteristic;
+
+    public function __construct(Characteristic $characteristic = null, $config = [])
+    {
+        if ($characteristic) {
+            $this->name = $characteristic->name;
+            $this->type = $characteristic->type;
+            $this->required = $characteristic->required;
+            $this->default = $characteristic->default;
+            $this->textVariants = implode(PHP_EOL, $characteristic->variants);
+            $this->sort = $characteristic->sort;
+            $this->_characteristic = $characteristic;
+        } else {
+            // если мы создаем новую характеристику то в сортировке указываем максимальное значение +1
+            $this->sort = Characteristic::find()->max('sort') + 1;
+        }
+        parent::__construct($config);
+    }
+
+    public function rules(): array
+    {
+        return [
+            [['name', 'type', 'sort'], 'required'],
+            [['required'], 'boolean'],
+            [['default'], 'string', 'max' => 255],
+            [['textVariants'], 'string'],
+            [['sort'], 'integer'],
+            [['name'], 'unique', 'targetClass' => Characteristic::class, 'filter' => $this->_characteristic ? ['<>', 'id', $this->_characteristic->id] : null]
+        ];
+    }
+
+    /** Разбиваем варианты на массив (для списка вариантов) */
+    public function getVariants(): array
+    {
+        return preg_split('#\s+#i', $this->textVariants);
+    }
+}
