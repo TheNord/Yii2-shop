674e3ac8dad7c05f56c1260f605468e01c5d7b54
thenord <dmr11@bk.ru>
2018-12-30 15:17:52 +0300

    Added blog tags

---
+++ backend/controllers/blog/TagController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace backend\controllers\blog;
+
+use shop\forms\manage\Blog\TagForm;
+use shop\services\manage\Blog\TagManageService;
+use Yii;
+use shop\entities\Blog\Tag;
+use backend\forms\Blog\TagSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
+class TagController extends Controller
+{
+    private $service;
+
+    public function __construct($id, $module, TagManageService $service, $config = [])
+    {
+        parent::__construct($id, $module, $config);
+        $this->service = $service;
+    }
+
+    public function behaviors(): array
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
+
+    public function actionIndex()
+    {
+        $searchModel = new TagSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'tag' => $this->findModel($id),
+        ]);
+    }
+
+    public function actionCreate()
+    {
+        $form = new TagForm();
+        if ($form->load(Yii::$app->request->post()) && $form->validate()) {
+            try {
+                $tag = $this->service->create($form);
+                return $this->redirect(['view', 'id' => $tag->id]);
+            } catch (\DomainException $e) {
+                Yii::$app->errorHandler->logException($e);
+                Yii::$app->session->setFlash('error', $e->getMessage());
+            }
+        }
+        return $this->render('create', [
+            'model' => $form,
+        ]);
+    }
+
+    public function actionUpdate($id)
+    {
+        $tag = $this->findModel($id);
+        $form = new TagForm($tag);
+        if ($form->load(Yii::$app->request->post()) && $form->validate()) {
+            try {
+                $this->service->edit($tag->id, $form);
+                return $this->redirect(['view', 'id' => $tag->id]);
+            } catch (\DomainException $e) {
+                Yii::$app->errorHandler->logException($e);
+                Yii::$app->session->setFlash('error', $e->getMessage());
+            }
+        }
+        return $this->render('update', [
+            'model' => $form,
+            'tag' => $tag,
+        ]);
+    }
+
+    public function actionDelete($id)
+    {
+        try {
+            $this->service->remove($id);
+        } catch (\DomainException $e) {
+            Yii::$app->errorHandler->logException($e);
+            Yii::$app->session->setFlash('error', $e->getMessage());
+        }
+        return $this->redirect(['index']);
+    }
+
+    protected function findModel($id): Tag
+    {
+        if (($model = Tag::findOne($id)) !== null) {
+            return $model;
+        }
+        throw new NotFoundHttpException('The requested page does not exist.');
+    }
+}
